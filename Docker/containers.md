47f9a0e42b793ee54673c19c9373487990c0e465
iamhasanimam <ihasanijs@gmail.com>
2025-09-27 12:41:24 +0530

    pushed from local

---
+++ Docker/containers.md
@@ -0,0 +1,10 @@
+### Containers
+
+What are containers ?
+
+Architecture of containers
+
+We can create containers on top of VM or physical machine as well
+
+We can talk from one container to another container
+
---
+++ Python/Lecture1.py
@@ -0,0 +1,46 @@
+import sys      
+
+print(sys.version)
+print("Hello Welocme to python ")
+
+if 5 < 2:
+    print("Five is greater than two !")
+else:
+    print("nothing")
+
+x = 5
+y = "hello world"
+
+print(str(x) + " " +y)
+
+# This is a coment in python]
+
+"""
+This is a multiple line comments
+"""
+print(type(x))
+print(type(y))
+
+z = ["apple", "bananan", "mango"]
+print(type(z))
+
+j = ("apple", "bananan", "mango")
+
+print(type(j))
+
+
+c = range(5)
+print(c)
+
+x1 = {"apple" : "Mera", "bananan":"mera", "mango":"tera"}
+print(x1.keys())
+print(x1.values())
+
+print(x1["apple"])
+print(x1["mango"])
+
+
+print(list(x1.keys())[0])
+print(list(x1.values())[0])
+
+print(list(x1.items())[0])
